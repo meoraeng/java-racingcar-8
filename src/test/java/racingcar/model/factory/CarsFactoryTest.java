d984a8a0d12357c27d21517b9cf8746aedede21a
SeonWu Kim <sunwoo005@naver.com>
2025-10-26 22:30:45 +0900

    test(CarsFactory): Create test for createCars success and fail case

---
+++ src/test/java/racingcar/model/factory/CarsFactoryTest.java
@@ -0,0 +1,33 @@
+package racingcar.model.factory;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import racingcar.model.Cars;
+import racingcar.model.validator.UniqueNamesValidator;
+import racingcar.model.validator.Validator;
+
+public class CarsFactoryTest {
+    @Test
+    @DisplayName("이름 배열을 받아 Cars를 생성")
+    void createCarsSuccess() {
+        Validator<String[]> validator = new UniqueNamesValidator();
+        CarsFactory factory = new CarsFactory(validator);
+
+        Cars cars = factory.createFrom(new String[]{"pobi", "woni"});
+
+        assertThat(cars).isNotNull();
+    }
+
+    @Test
+    @DisplayName("중복된 이름이 있으면 예외 발생")
+    void createCarsFailureForDuplicateNamesCar() {
+        Validator<String[]> validator = new UniqueNamesValidator();
+        CarsFactory factory = new CarsFactory(validator);
+
+        assertThat(() -> factory.createFrom(new String[]{"pobi", "pobi"}))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("중복");
+    }
+}
